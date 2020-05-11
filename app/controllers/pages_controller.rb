89542bdfd123357d7f6460baa47c536e7ca24b50
Edgar Minault <e.minault@bluewin.ch>
2020-05-11 12:09:33 +0200

    Question and answer displayed.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,21 @@
 class PagesController < ApplicationController
   def ask
-    @question = params[:question]
   end
 
-  def answers
+  def answer
+    @question = params[:question]
+    specified_answer
+  end
+
+  private
+
+  def specified_answer
+    if @question.downcase == "i am going to work"
+      @answer = "Great!"
+    elsif @question.split("").last == "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
